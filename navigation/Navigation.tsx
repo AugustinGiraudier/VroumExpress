1968da4315283b2885c0558302f7bc625cbf9e79
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-05-22 09:27:36 +0200

    icons tab bar

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import OptionScreen from "../screens/OptionScreen";
+import Icon from 'react-native-vector-icons/FontAwesome';
 import React from "react";
 
 export default function Navigation() {
@@ -10,13 +11,19 @@ export default function Navigation() {
         <NavigationContainer>
             <BottomTabNavigator.Navigator initialRouteName="Home">
                 <BottomTabNavigator.Screen name="Home" component={HomeScreen}
-                                           options={{
-                                               title: 'Home',
-                                           }}/>
+                   options={{
+                       title: 'Home',
+                       tabBarIcon: ({ color, size }) => (
+                           <Icon name="home" color={color} size={size} />
+                       ),
+                   }}/>
                 <BottomTabNavigator.Screen name="Option" component={OptionScreen}
-                                           options={{
-                                               title: 'Option',
-                                           }}/>
+                   options={{
+                       title: 'Option',
+                       tabBarIcon: ({ color, size }) => (
+                           <Icon name="cog" color={color} size={size} />
+                       ),
+                   }}/>
             </BottomTabNavigator.Navigator>
         </NavigationContainer>
     )
