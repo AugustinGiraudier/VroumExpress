7812dd6ce7631e08d3941bdb81d11748ec00eda1
thibault.nicolas <thibault.nicolas@etu.uca.fr>
2023-05-26 09:44:06 +0200

    Correction style OptionsScreen

--- screens/OptionsScreen.tsx
+++ screens/OptionsScreen.tsx
@@ -6,9 +6,7 @@ import styles from '../styles/main';
 export default function OptionsScreen() {
     return (
       <View style={styles.container}>
-        <View style={styles.centered}>
-          <Text style={[styles.title,styles.text]}>Option</Text>
-        </View>
+        <Text style={styles.title}>Option</Text>
         <Text style={styles.text}>Page d'options</Text>
       </View>
     )
