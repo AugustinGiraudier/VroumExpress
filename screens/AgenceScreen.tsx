406adbbe6132056ee7305ce6b28d131c9ac1d42c
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-05-26 08:36:04 +0200

    merge styles exportés

--- screens/AgenceScreen.tsx
+++ screens/AgenceScreen.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styles from '../styles/main';
+
 export default function HomeScreen() {
     return (
       <View style={styles.container}>
         <View style={styles.centered}>
           <Text style={styles.title}>Mes super Nounours !</Text>
         </View>
-        <Text>Mon super texte ...</Text>
+        <Text style={styles.text}>Mon super texte ...</Text>
       </View>
     )
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "darksalmon",
-    },
-    centered: {
-      alignItems: "center"
-    },
-    title: {
-      fontSize: 20
-    }
-  });
--- styles/main.js
+++ styles/main.js
@@ -2,14 +2,18 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: "darksalmon",
+        flex: 1,
+        backgroundColor: "#FE5F00",
     },
     centered: {
-      alignItems: "center"
+        alignItems: "center"
     },
     title: {
-      fontSize: 20
+        fontSize: 20,
+        color:"white"
+    },
+    text:{
+        color:"white"
     }
   });
 
