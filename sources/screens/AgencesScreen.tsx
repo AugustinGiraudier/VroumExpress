b7e6127f0f63efe989aeacbbf56eeb4deb3af252
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-06-07 09:12:19 +0200

    scroll view page agences

--- sources/screens/AgencesScreen.tsx
+++ sources/screens/AgencesScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from '../styles/main';
@@ -22,34 +22,60 @@ export default function AgencesScreen() {
             agence: 'Marseille',
             dispo:"200 véhicules dispo"
         },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d73',
+            agence: 'Marseille',
+            dispo:"200 véhicules dispo"
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d74',
+            agence: 'Marseille',
+            dispo:"200 véhicules dispo"
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d75',
+            agence: 'Marseille',
+            dispo:"200 véhicules dispo"
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d76',
+            agence: 'Marseille',
+            dispo:"200 véhicules dispo"
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d77',
+            agence: 'Marseille',
+            dispo:"200 véhicules dispo"
+        },
     ];
 
 
     return (
         <View style={styles.mainContainer}>
+            <ScrollView style={{width:"100%"}}>
+                <Text style={[styles.text, {marginTop:20}]}>Trouver une agence</Text>
+                <TextInput
+                    style={styles.input}
+                    value=""
+                    placeholder="Ex : Paris"
+                />
 
-            <Text style={[styles.text, {marginTop:20}]}>Trouver une agence</Text>
-            <TextInput
-                style={styles.input}
-                value=""
-                placeholder="Ex : Paris"
-            />
 
-
-            <Text style={[styles.text, {marginTop:20}]}>Les principales</Text>
-            <View style={[styles.container]}>
-                <FlatList style={{width:"100%"}} data={DATA} keyExtractor={item => item.id} renderItem={({item}) =>
-                    <View style={styles.lightContainer}>
-                        <View style={styles.hStack}>
-                            <Icon name="home" color={styles.mainContainer.backgroundColor} size={30} />
-                            <View style={{marginLeft:8}}>
-                                <Text style={styles.title}>{item.agence}</Text>
-                                <Text style={styles.textSecondary}>{item.dispo}</Text>
+                <Text style={[styles.text, {marginTop:20}]}>Les principales</Text>
+                <View style={[styles.container]}>
+                    <FlatList style={{width:"100%"}} data={DATA} keyExtractor={item => item.id} renderItem={({item}) =>
+                        <View style={styles.lightContainer}>
+                            <View style={styles.hStack}>
+                                <Icon name="home" color={styles.mainContainer.backgroundColor} size={30} />
+                                <View style={{marginLeft:8}}>
+                                    <Text style={styles.title}>{item.agence}</Text>
+                                    <Text style={styles.textSecondary}>{item.dispo}</Text>
+                                </View>
                             </View>
                         </View>
-                    </View>
-                }/>
-            </View>
+                    }/>
+                </View>
+            </ScrollView>
         </View>
     )
   };
